43351e88387a689a9050e9e31efe667f9d565fee
Mena Wahba <menawahba@gmail.com>
2021-04-18 19:04:49 -0400

    adding changes

--- models/workouts.js
+++ models/workouts.js
@@ -1,50 +1,57 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-    day: {
-        type: Date,
-        default: Date.now
-    },
-
-    exercises: [
-        {
-
-            type: {
-                type: String,
-                required: "Type of exercise is required"
-            },
-
-            name: {
-                type: String,
-                required: "Name of exercise is required"
-            },
-
-            duration: {
-                type: Number,
-                required: "Duration is required"
-            },
-
-            weight: {
-                type: Number,
-            },
-
-            reps: {
-                type: Number,
-            },
-
-
-            sets: {
-                type: Number,
-            },
-
-            distance: {
-                type: Number,
+const workoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: () => new Date()
+        },
+        exercises: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: "Enter an exercise type"
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "Enter an exercise name"
+                },
+                duration: {
+                    type: Number,
+                    required: "Enter an exercise duration in minutes"
+                },
+                weight: {
+                    type: Number
+                },
+                reps: {
+                    type: Number
+                },
+                sets: {
+                    type: Number
+                },
+                distance: {
+                    type: Number
+                }
             }
+        ]
+    },
+    {
+        toJSON: {
+            // include any virtual properties when data is requested
+            virtuals: true
         }
+    }
+);
 
-    ]
-
+// adds a dynamically-created property to schema
+workoutSchema.virtual("totalDuration").get(function () {
+    // "reduce" array of exercises down to just the sum of their durations
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 const express = require("express");
+const morgan = require("morgan");
 const mongoose = require("mongoose");
-const logger = require("morgan");
-
-const PORT = process.env.PORT || 3000;
 const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.use(logger("dev"));
+app.use(morgan("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
-app.use(express.static("public"));
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useFindAndModify: false });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false
+})
 
-app.use(require("./routes/api.js"));
-app.use(require("./routes/routes.js"));
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+app.listen(PORT, function () {
+    console.log(`App listening on Port ${PORT}`);
 });
