e8820b260e37d4ee6b8e4186aafc668173d1a17b
Mena Wahba <menawahba@gmail.com>
2021-04-19 21:26:04 -0400

    still not working despite some of the changes

--- models/workout.js
+++ models/workout.js
@@ -39,11 +39,11 @@ const workoutSchema = new Schema({
     ]
 });
 
-workoutSchema.virtual("totalDuration").get(function () {
-    return this.exercises.reduce((total, exercise) => {
-        return total + exercise.duration;
-    }, 0);
-});
+// workoutSchema.virtual("totalDuration").get(function () {
+//     return this.exercises.reduce((total, exercise) => {
+//         return total + exercise.duration;
+//     }, 0);
+// });
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
--- server.js
+++ server.js
@@ -12,14 +12,14 @@ app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-
+app.use(htmlRoutes);
+app.use(apiRoutes);
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
     useFindAndModify: false
 });
 
-app.use(htmlRoutes);
-app.use(apiRoutes);
+
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+    console.log(`App listening at http://localhost:${PORT}.`);
 });
